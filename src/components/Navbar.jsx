b61646b63b5247736600648ef6bdce0d9124e824
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-13 16:05:03 -0500

    Setting up nav, adding nav details

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+
+        </div>
+        {/* menu */}
+        <div>
+            <ul>
+                <li>About</li>
+                <li>Home</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Resume</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
+    </div>
   )
 }
 

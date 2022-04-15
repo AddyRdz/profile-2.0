b2b1e42627151ed0e302058f89109d98da21dfcd
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-14 22:47:08 -0500

    Added some personal about me info.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,16 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#1b1ba5]'>Home</div>
+    <div name='home' className='w-full h-screen bg-[#1b1ba5]'>
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p>Hi, my name is</p>
+            <h1>Addy Rodriguez</h1>
+            <h2>I'm a Full Stack Developer.</h2>
+            <p>WWelcome to my portfolio, I am starting out my career as a software engineer. I just graduated from General Assembly and I am looking forward to applying everything I have learned. Please look around and make sure to reach out!</p>
+
+        </div>
+    </div>
   )
 }
 

1d91c308ec7d6efb9632ac6a7f580ed885ad5517
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-16 00:10:54 -0500

    Set up icon image on skill section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,9 +4,9 @@ import JAVASCRIPT from '../assets/javascript.png';
 import CSS from '../assets/css3.png';
 import PYTHON from '../assets/python.png';
 import REACT from '../assets/react.png';
-import Node from '../assets/node-js.png';
-import Bootstrap from '../assets/bootstrap.png';
-import Tailwind from '../assets/tailwind.png';
+import NODE from '../assets/node-js.png';
+import BOOTSTRAP from '../assets/bootstrap.png';
+import TAILWIND from '../assets/tailwind.png';
 
 const Skills = () => {
   return (
@@ -17,12 +17,40 @@ const Skills = () => {
                 <p className='text-4xl font-bold inline border-b-4'>Skills</p>
                 <p className='py-4'>These are the technologies I've dabbled in:</p>
             </div>
-            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                <div>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                 <img className='w-20 mx-auto' src={HTML} alt="HTML icon"/>
-                <p>HTML</p>
-                </div>
+                <p className='my-4'>HTML</p>
             </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={JAVASCRIPT} alt="JAVASCRIPT icon"/>
+                <p className='my-4'>JAVASCRIPT</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={CSS} alt="CSS icon"/>
+                <p className='my-4'>CSS</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={PYTHON} alt="PYTHON icon"/>
+                <p className='my-4'>PYTHON</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={REACT} alt="REACT icon"/>
+                <p className='my-4'>REACT</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={NODE} alt="NODE icon"/>
+                <p className='my-4'>NODE</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={BOOTSTRAP} alt="BOOTSTRAP icon"/>
+                <p className='my-4'>BOOTSTRAP</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={TAILWIND} alt="TAILWIND icon"/>
+                <p className='my-4'>TAILWIND</p>
+            </div>
+        </div>
         </div>
     </div>
   )

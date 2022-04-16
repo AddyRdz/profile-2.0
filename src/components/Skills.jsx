d0b488e3623fe5fd0ded689c9b003eb206f4d3bf
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-15 22:48:26 -0500

    imported images for skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import HTML from '../assets/html.png';
+import JAVASCRIPT from '../assets/javascript.png';
+import CSS from '../assets/css3.png';
+import PYTHON from '../assets/python.png';
+import REACT from '../assets/react.png';
+import Node from '../assets/node-js.png';
+import Bootstrap from '../assets/bootstrap.png';
+import Tailwind from '../assets/tailwind.png';
+
+const Skills = () => {
+  return (
+    <div>Skills</div>
+  )
+}
+
+export default Skills

161f23180b2a24a51989d0cbeb11f61c178e50cd
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-16 01:00:43 -0500

    Beginning on projects shown on screen

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,28 @@ import React from 'react'
 export const Projects = () => {
   return (
     <div name='work' className='w-full md:h-screen text-[#ccd6f6] bg-[#1b1ba5]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w=full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4'>Projects</p>
+            <p>Here is some of my recent work.</p>
+        </div>
         <div>
-            <p>Projects</p>
+            <div>
+                <div>
+                    <span>
+
+                    </span>
+                    <div>
+                        <a href="/">
+                            <button></button>
+                        </a>
+                        <a href="/">
+                            <button></button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     </div>
   )

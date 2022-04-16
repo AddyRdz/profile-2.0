cc7b4c410bfb513d8856382f9cf74cd6d5b904e9
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-15 23:38:49 -0500

    Some styling to title, skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,15 +10,15 @@ import Tailwind from '../assets/tailwind.png';
 
 const Skills = () => {
   return (
-    <div name='skills'>
+    <div name='skills' className='bg-[#1b1ba5] text-[#ccd6f6]'>
         {/* Container */}
-        <div>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
-                <p>Skills</p>
+                <p className='text-4xl font-bold inline border-b-4'>Skills</p>
                 <p>These are the technologies I've dabbled in:</p>
             </div>
             <div>
-                <img src={HTML} alt="HTML icon"/>
+                <img className='w-20 mx-auto' src={HTML} alt="HTML icon"/>
                 <p>HTML</p>
             </div>
         </div>

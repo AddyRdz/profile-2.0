6a8d7b4d5184b4e51e3fb431f31c6e16ed641b91
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-16 23:26:23 -0500

    Started working on project container

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,10 +6,12 @@ export const Projects = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w=full h-full'>
         <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4'>Projects</p>
-            <p>Here is some of my recent work.</p>
+            <p className='py-6'>Here is some of my recent work.</p>
         </div>
-        <div>
-            <div>
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+            <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                {/* Hover Effects */}
                 <div>
                     <span>
 

08d9387c435bb3afeb494d3610625ffccab24229
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-17 14:21:55 -0500

    Resume component is showing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#1b1ba5] flex justify-center items-center p-4'>
-        <form action="" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/5661cd29-b3f3-4528-9b8f-03d95c5c6243" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-[#ccd6f6]'>Contact</p>
-                <p className='text=[#ccd6f6]'> You can reach me by submitting the form below or you can send me an email to addyrodriguez18@gmail.com</p>
+                <p className='text-[#ccd6f6] py-4'> You can reach me by submitting the form below or you can send me an email to addyrodriguez18@gmail.com</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name'/>
             <input className='my-4 p-2 bg-[#ccd6f6]'type="email" placeholder='Email' name='email'/>
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Resume = () => {
   return (
-    <div className='w-full h-screen bg-[#1b1ba5]'>Resume</div>
+    <div name='resume' className='w-full h-screen bg-[#1b1ba5] text-[#ccd6f6]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4'>Resume</p>
+            </div>
+        </div>
+    </div>
   )
 }
 

12e57c26cb710c80d18c4b5f8c3066121ad5b7e1
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-17 15:44:07 -0500

    Corrected div name for nav to work

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ import MyPetPro from '../assets/MyPetPro.jpeg'
 
 export const Projects = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-[#ccd6f6] bg-[#1b1ba5]'>
+    <div name='projects' className='w-full md:h-screen text-[#ccd6f6] bg-[#1b1ba5]'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w=full h-full'>
         <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4'>Projects</p>

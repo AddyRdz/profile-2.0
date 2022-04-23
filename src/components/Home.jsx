6d4064e6c53bd9ab51c1d7886a10cdd61bd22151
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-23 16:51:36 -0500

    Projects button scrolls

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import { Link } from 'react-scroll/modules'
 
 const Home = () => {
   return (
@@ -11,7 +12,9 @@ const Home = () => {
             <h2 className="text-4xl sm:text-3xl font-bold text-[#ccd6f6]">I'm a Full Stack Developer.</h2>
             <p className='font-bold text-[#8f9ecc] py-4 max-w-[700px]'>Welcome to my portfolio, I am starting out my career as a software engineer. I just graduated from General Assembly and I am looking forward to applying everything I have learned. Please look around and make sure to reach out!</p>
             <div>
-                <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-black'>See my work<HiArrowNarrowRight className='ml-4'/></button>
+              <Link to="projects" smooth={true} offset={50} duration={500}>
+                <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-black'>See my projects<HiArrowNarrowRight className='ml-4'/></button>
+              </Link> 
             </div>
 
         </div>

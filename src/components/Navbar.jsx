d6d64c19de4dc69daf9ffee6dad11e02a0c8c597
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-23 12:48:15 -0500

    Added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Logo from '../assets/AR Logo.png'
 import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs'
@@ -11,7 +12,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-
+            <img src={Logo} alt="Logo Image" style={{width:'70px'}}/>
         </div>
         {/* menu */}
             <ul className='hidden md:flex'>

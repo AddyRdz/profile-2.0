f3db770e6ac48412f1bfb7b9fdfb304b9d6173f3
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-23 15:28:43 -0500

    Logo links to home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,9 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
+            <Link to="home" smooth={true} offset={50} duration={500}>
             <img src={Logo} alt="Logo Image" style={{width:'70px'}}/>
+            </Link>
         </div>
         {/* menu */}
             <ul className='hidden md:flex'>

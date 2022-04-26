6bf7adaab93da0504232dbbecb62eb2dfd1e58fc
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-25 19:44:18 -0500

    Saved some footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div name='Footer' className='flex flex-col items-center justify-evenly px-6 py-4 text-[#ccd6f6] bg-[#1b1ba5]'>
+        By Addy Rodriguez
+    </div>
   )
 }
 
